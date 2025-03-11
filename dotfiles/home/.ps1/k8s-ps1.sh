ec326a7c8be742847d1f9ac3abc5697864de4035
Chitchai <chitchaic@gmail.com>
2025-03-11 14:18:47 +0700

    Fix

--- dotfiles/home/.ps1/k8s-ps1.sh
+++ dotfiles/home/.ps1/k8s-ps1.sh
@@ -1,5 +1,66 @@
 #!/bin/bash
 
+function user_color {
+  p_user="$HOSTNAME"
+
+  is_root=$(whoami)
+  if [[ "${is_root}" == "root" ]]; then
+    export usercolor="${bold}${fg_red}"
+  else
+    export usercolor="${bold}${fg_yellow}"
+  fi
+}
+
+function k8s_user_color {
+  # Set default (dummy) KUBECONFIG
+  if [[ -z $KUBECONFIG ]]; then
+    # export KUBECONFIG="${HOME}/.kube/config"
+    k8s_usercolor=""
+    p_k8suser=""
+    p_namespace=""
+  fi
+
+  # kubeconfig per session
+  if [[ ! ${KUBECONFIG} =~ /tmp/kubectx.* ]]; then
+    kubeconfig_tmpfile="$(mktemp -t "kubectx.XXXXXX")"
+
+    if [[ -f ${KUBECONFIG} ]]; then
+      cp ${KUBECONFIG} ${kubeconfig_tmpfile}
+      export KUBECONFIG=${kubeconfig_tmpfile}
+    fi
+  fi
+
+  # Unset KUBECONFIG if no contexts
+  if [[ -z $(yq e '.contexts //""' $KUBECONFIG) ]]; then
+    cp $KUBECONFIG ~/.kube/config
+    unset KUBECONFIG
+  fi
+
+  if [[ -n $KUBECONFIG  && -f $KUBECONFIG ]]; then
+    default_kubectx=$(yq e '.contexts[0].name' $KUBECONFIG | sed 's/null//')
+    export selected_kubectx=${KUBECTX:-$default_kubectx}
+
+    # Noted. $KUBECTX is from kubectx
+    # edit current context from $KUBECTX
+    # yq e -i '.current-context=strenv(selected_kubectx)' $KUBECONFIG
+
+    ps1_namespace=$(yq e '.contexts[]?|select(.name==env(selected_kubectx)).context.namespace' $KUBECONFIG | sed 's/null/default/')
+    p_namespace=$(echo " ($ps1_namespace)")
+
+    # TODO Bug
+    export p_k8suser=$(yq e '.contexts[]?|select(.name==env(selected_kubectx)).context.cluster' $KUBECONFIG)
+    export p_k8suser=${p_k8suser:-$USER}
+
+    is_k8s_prod=$(yq e '.preferences.production' $KUBECONFIG)
+    [[ $is_k8s_prod == "true" ]] && export k8s_usercolor=${bold}${fg_red} || export k8s_usercolor=${bold}${fg_yellow}
+  fi
+
+  if [[ -n "$k8s_usercolor" ]]; then
+    export usercolor=${k8s_usercolor}
+    export p_user="${p_k8suser}"
+  fi
+}
+
 
 ## - Function select eks-cluster
 select_eks_cluster() {
